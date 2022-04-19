6f2b39c62e6ad3d908c788e65cabf29e73944b9c
Allan <altonallan47@gmail.com>
2022-04-19 10:51:40 -0700

    Added the fetch function
    I then added the fetch function following it's response in json format by simple GET method then, added the output to the innerHTML but the time ran out before i could actually modify the code to work with the HTML IDs (i was thinking of using the append method Async/Await, but while i was still processing my thoughts time ran out so, i saw it wise to be faithful and just submit whatever i had'😁)

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 const getRandomUserData = (country) => {
     
-   
+    //Get API
+    fetch('https://randomuser.me/api/?nat=${country}') //?nat=us,dk,fr,gb
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+
+            let author = data.results;
+            // console.log(author);
+
+            //Get Data Value
+            let output = "<h2><center>Get User Data</center></h2>";
+
+            //Get Data Loop Through
+            author.forEach(function (lists) {
+                output += `
+                <div class="container">
+                    <div class="card mt-4 bg-light">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h2>Name: ${lists.name.first}</h2></li>
+                            <li class="list-group-item">Email ID: ${lists.email}</li>
+                            <li class="list-group-item">Gender: ${lists.gender}</li>
+                            <li class="list-group-item">Country: ${lists.location.country}</li>
+                        </ul>
+                    </div>
+                </div> `;
+            });
+           
+
+
+            //Show On Our Screen All Data
+            document.getElementById('output').innerHTML = output;
+
+        });
 };
 
 
